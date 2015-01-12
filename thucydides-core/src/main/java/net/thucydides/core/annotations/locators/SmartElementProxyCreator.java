51868d5b597cdd278587bc730ac8cf70ec2f5ffb
Anton Ostrenko <anton_ostrenko@epam.com>
2015-01-12 16:57:51 +0300

    added htmlElements

--- thucydides-core/src/main/java/net/thucydides/core/annotations/locators/SmartElementProxyCreator.java
+++ thucydides-core/src/main/java/net/thucydides/core/annotations/locators/SmartElementProxyCreator.java
@@ -10,28 +10,30 @@ import net.thucydides.core.pages.PageObject;
 import net.thucydides.core.webdriver.Configuration;
 import net.thucydides.core.webdriver.ElementLocatorFactorySelector;
 import net.thucydides.core.webdriver.ElementProxyCreator;
+import ru.yandex.qatools.htmlelements.loader.HtmlElementLoader;
 
 public class SmartElementProxyCreator implements ElementProxyCreator {
 
-	@Override
-	public void proxyElements(PageObject pageObject, WebDriver driver) {
-		ElementLocatorFactory finder = getElementLocatorFactorySelector().getLocatorFor(driver);
-        FieldDecorator decorator = new SmartFieldDecorator(finder, driver, pageObject);
-        PageFactory.initElements(decorator, pageObject);
+    @Override
+    public void proxyElements(PageObject pageObject, WebDriver driver) {
+//		ElementLocatorFactory finder = getElementLocatorFactorySelector().getLocatorFor(driver);
+//        FieldDecorator decorator = new SmartFieldDecorator(finder, driver, pageObject);
+//        PageFactory.initElements(decorator, pageObject);
+        HtmlElementLoader.populatePageObject(pageObject, driver);
+    }
 
-	}
+    @Override
+    public void proxyElements(PageObject pageObject, WebDriver driver, int timeoutInSeconds) {
+//		ElementLocatorFactory finder = getElementLocatorFactorySelector().withTimeout(timeoutInSeconds).getLocatorFor(driver);
+//        FieldDecorator decorator = new SmartFieldDecorator(finder, driver, pageObject);
+//        PageFactory.initElements(decorator, pageObject);
+        HtmlElementLoader.populatePageObject(pageObject, driver);
 
-	@Override
-	public void proxyElements(PageObject pageObject, WebDriver driver, int timeoutInSeconds) {
-		ElementLocatorFactory finder = getElementLocatorFactorySelector().withTimeout(timeoutInSeconds).getLocatorFor(driver);
-        FieldDecorator decorator = new SmartFieldDecorator(finder, driver, pageObject);
-        PageFactory.initElements(decorator, pageObject);
+    }
 
-	}
-	
-	private ElementLocatorFactorySelector getElementLocatorFactorySelector() {
-	    Configuration configuration = Injectors.getInjector().getInstance(Configuration.class);
-	    return new ElementLocatorFactorySelector(configuration);
-	}
+    private ElementLocatorFactorySelector getElementLocatorFactorySelector() {
+        Configuration configuration = Injectors.getInjector().getInstance(Configuration.class);
+        return new ElementLocatorFactorySelector(configuration);
+    }
 
 }
